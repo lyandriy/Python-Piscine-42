9868886fabd5baf6ce47bcbde0edddacd87ef0fe
lyandriy <lyudmyla1829@gmail.com>
2025-03-01 20:28:17 +0100

    ex02-

--- Module_01/ex02/test.py
+++ Module_01/ex02/test.py
@@ -1,7 +1,33 @@
 from vector import Vector
 
 if __name__ == "__main__":
-    vector = Vector([[0.0], [1.0], [2.0], [3.0]])
-    print(vector)
-    for x in vector:
-        print(x)
+    
+    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    v2 = v1 * 5
+    print(v2)
+    
+    v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+    v2 = v1 * 5
+    print(v2)
+    
+    v2 = v1 / 2.0
+    print(v2)
+    
+    #v1 / 0.0
+    
+    #2.0 / v1
+    
+    print(Vector([[0.0], [1.0], [2.0], [3.0]]).shape)
+    
+    print(Vector([[0.0], [1.0], [2.0], [3.0]]).values)
+    
+    print(Vector([[0.0, 1.0, 2.0, 3.0]]).shape)
+    
+    print(Vector([[0.0, 1.0, 2.0, 3.0]]).values)
+    
+    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    print(v1.shape)
+    
+    print(v1.T())
+    
+    print(v1.T().shape)
--- Module_01/ex02/vector.py
+++ Module_01/ex02/vector.py
@@ -3,49 +3,131 @@ class Vector:
         self.values = []
         self.shape = (0, 0)
         if isinstance(values, list):
-            if isinstance(values[0], float):
-                self.values = values
-                self.shape = (len(self.values), len(self.values[0]))
-            elif isinstance(values[0], list):
-                print("HOLAAAAA")
+            if isinstance(values[0][0], float) or isinstance(values[0][0], list):
                 self.values = values
                 self.shape = (len(self.values), len(self.values[0]))
             else:
-                print("Error")
+                raise ValueError("ERROR")
         elif isinstance(values, int):
             for x in range(values):
                 self.values.append([float(x)])
             self.shape = (len(self.values), 1)
         elif isinstance(values, tuple):
             if len(values) != 2 or not isinstance(values[0], int) or not isinstance(values[1], int) or values[0] > values[1]:
-                print("Error")
+                raise ValueError("ERROR")
             else:
                 x = values[0]
-                while x <= values[1]:
+                while x < values[1]:
                     self.values.append([float(x)])
                     x += 1
                 self.shape = (len(self.values), 1)
         else:
-            print("Error")
+            raise ValueError("ERROR")
+    
+    
+    # __add__  __radd__ add & radd : only vectors of same shape.
+    def __add__(self, other):
+        if isinstance(other, Vector) and self.shape == other.shape:
+            if self.shape[0] == 1:#es una lista
+                vector = []
+                for i in range(self.shape[1]):
+                     vector.append(self.values[0][i] + other.values[0][i])
+                return Vector(vector)
+            else:
+                vector = []
+                for i in range(len(self.values)):
+                    vector.append([(self.values[i][0] + other.values[i][0])])
+                return Vector(vector)
+        else:
+            raise ValueError("ERROR")
+        
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    #__sub__ __rsub__ sub & rsub: only vectors of same shape.
+    def __sub__(self, other):
+        if isinstance(other, Vector) and self.shape == other.shape:
+            if self.shape[0] == 1:
+                vector = []
+                for i in range(self.shape[1]):
+                     vector.append(self.values[0][i] - other.values[0][i])
+                return Vector(vector)
+            else:
+                vector = []
+                for i in range(len(self.values)):
+                    vector.append([(self.values[i][0] - other.values[i][0])])
+                return Vector(vector)
+        else:
+            raise ValueError("ERROR")
+        
+    def __rsub__(self, other):
+        return self.__sub__(other)
+    
+    #__mul__ __rmul__ mul & rmul: only scalars (to perform multiplication of Vector by a scalar).   
+    def __mul__(self, scalar):
+        if isinstance(scalar, (int, float)):
+            new_vector = []
+            if self.shape[0] == 1:
+                new_vector.append([self.values[0][i] * scalar for i in range(self.shape[1])])
+                return Vector(new_vector)
+            else:
+                return Vector([[self.values[i][0] * scalar] for i in range(self.shape[0])])
+        else:
+            raise ValueError("ERROR")
+    
+    def __rmul__(self, scalar):
+        return self.__mul__(scalar)
+    
+    #__truediv__ truediv : only with scalars (to perform division of Vector by a scalar).
+    #__rtruediv__ rtruediv : raises an NotImplementedError with the message "Division of a scalar by a Vector is not defined here."
+    
+    def __truediv__(self, scalar):
+        if isinstance(scalar, (int, float)):
+            if scalar == 0 or scalar == 0.0:
+                raise ZeroDivisionError("Cannot be divided by zero.")
+            new_vector = []
+            if self.shape[0] == 1:
+                new_vector.append([self.values[0][i] / scalar for i in range(self.shape[1])])
+            else:
+                new_vector.append([[self.values[i][0] / scalar] for i in range(self.shape[0])])
+            return Vector(new_vector)
+        else:
+            raise ValueError("ERROR")
+    
+    def __rtruediv__(self, scalar):
+        raise NotImplementedError ("Division of a scalar by a Vector is not defined here.")
+    
+    #__str__ __repr__  must be identical, i.e we expect that print(vector) and vector within python interpretor behave the same, see correspondi
+    def __repr__(self):
+        return f"Vector({self.values})"
+    
+    def __str__(self):
+        return f"Vector({self.values})"
+
+    def __len__(self):
+        return len(self.values)
+
+    def __getitem__(self, index):
+        return self.values[index]
 
     def dot(self, other):
         if isinstance(other, Vector) and self.shape == other.shape:
             if self.shape[0] == 1:#es una lista
-                sum(self.values[0][i] * other.values[0][i] for i in range(len(self.shape[1])))
+                return sum(self.values[0][i] * other.values[0][i] for i in range(self.shape[1]))
             else:#varias listas
-                sum(self.values[i][0] * other.values[i][0] for i in range(len(self.values)))
+                return sum(self.values[i][0] * other.values[i][0] for i in range(len(self.values)))
         else:
-            print("Error")
+            raise ValueError("ERROR")
 
     
     def T(self):
         new_values = []
+        print(self.shape)
         if self.shape[0] == 1:#una [[1., 2., 3.]]
             for x in self.values[0]:
                 new_values.append([x])
         if self.shape[1] == 1:#varias [[0.0], [1.0], [2.0], [3.0]]
-            new_list = []
-            for x in self.values[0]:
-                new_list.append(x[0])
-            new_values.append(new_list)
-        return new_values
+            for x in self.values:
+                print(x)
+                new_values.append(x)
+        return Vector(new_values)
