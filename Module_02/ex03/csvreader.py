c2dab6c2e497712edf24cf871ee56690781d4234
lyandriy <lyudmyla1829@gmail.com>
2025-03-16 22:19:57 +0100

    csv

--- Module_02/ex03/csvreader.py
+++ Module_02/ex03/csvreader.py
@@ -1,24 +1,45 @@
 class CsvReader():
     def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
-        # ... Your code here ...
+        self.filename = filename
+        self.sep = sep
+        self.header = header
+        self.skip_top = skip_top
+        self.skip_bottom = skip_bottom
+        self.f
+        self.header_data
+        self.lines
+
+    def __enter__(self):
+        try:
+            self.f = open(self.filename, "r")
+            line = f.readline()
+            self.lines = []
+            while line:
+                self.lines.append(line.split(self.sep).strip())
+                line = f.readline()
+            size = 0
+            if self.header:
+                self.header_data = self.lines[0]
+                size = len(self.lines[0])
+                self.lines.pop(0)
+            del self.lines[0:self.skip_top]
+            del self.lines[self.skip_bottom:len(self.lines)]
+            while line in self.lines:
+                if size != len(line):
+                    raise
+        except ValueError:
+            print("Bad file")
         
-    def __enter__(...):
-        # ... Your code here ...
-        
-    def __exit__(...):
-        # ... Your code here ...
+    def __exit__(self):
+        if self.f:
+            self.f.close()
         
     def getdata(self):
-    """ Retrieves the data/records from skip_top to skip bottom.
-    Return:
-        nested list (list(list, list, ...)) representing the data.
-    """
-        # ... Your code here ...
+        if self.lines:
+            return self.lines
         
     def getheader(self):
-    """ Retrieves the header from csv file.
-    Returns:
-        list: representing the data (when self.header is True).
-    None: (when self.header is False).
-    """
-        # ... Your code here ...
+        if self.header_data:
+            return self.header_data
+        else:
+            None
